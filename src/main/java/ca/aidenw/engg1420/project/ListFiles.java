68bbd750569d8a8a3fbc46d92804a0f1ced8e21c
lalhajal <116668554+lalhajal@users.noreply.github.com>
2023-04-05 12:07:15 -0400

    Add files via upload

--- src/main/java/ca/aidenw/engg1420/project/ListFiles.java
+++ src/main/java/ca/aidenw/engg1420/project/ListFiles.java
@@ -1,16 +1,94 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package ca.aidenw.engg1420.project;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
+ * This takes in a list of directory entries and a maximum value, and returns a list of selected entries from each 
+ *      directory in the input list. It first creates two directories with files for testing, then calls a method 
+ *      that selects up to Max entries from each directory, and returns them as a list.
  * @author Lucy
- * @author Leonardo
- * @author Daniel
- * @author Vanessa
- * @author Aiden
  */
-public class ListFiles extends ProcessingElement {
-    
+public class ListFiles {
+    /**
+     * A method that takes in a list of directory entries and an integer Max, and returns a list of selected entries.
+     *
+     * @param entries The list of directory entries to process.
+     * @param Max     The maximum number of entries to select from each directory.
+     * @return A list of selected entries from the directories in the input list.
+     */
+    public static List<File> selectEntries(List<File> entries, int Max) {
+        // Initialize an empty list to hold the selected entries.
+        List<File> selectedEntries = new ArrayList<>(); 
+
+        // Loop through each entry in the input list.
+        for (File entry : entries) { 
+            // Check if the entry is a directory.
+            if (entry.isDirectory()) { 
+                // Get an array of all the files in the directory.
+                File[] files = entry.listFiles(); 
+
+                int numFiles = files.length;
+                // Determine the number of files to select from the directory.
+                int numSelectedFiles = Math.min(numFiles, Max); 
+                
+                // Loop through the selected files in the directory.
+                for (int i = 0; i < numSelectedFiles; i++) { 
+                    // Add the selected file to the output list.
+                    selectedEntries.add(files[i]); 
+                }
+            }
+        }
+        
+        // Return the list of selected entries.
+        return selectedEntries; 
+    }
+
+    /**
+     * A main method to test the selectEntries method.
+     * @param args Command-line arguments (not used in this case).
+     */
+    public static void main(String[] args) {
+        // Create a directory object for testing.
+        File directory1 = new File("directory1"); 
+        // Make the directory if it doesn't exist.
+        directory1.mkdirs(); 
+        
+        // Create 10 files in directory1.
+        for (int i = 1; i <= 10; i++) { 
+            File file = new File(directory1, "file" + i + ".txt");
+            try {
+                file.createNewFile();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        
+        // Create another directory object for testing.
+        File directory2 = new File("directory2"); 
+        // Make the directory if it doesn't exist.
+        directory2.mkdirs(); 
+        
+        // Create 50 files in directory2.
+        for (int i = 1; i <= 50; i++) { 
+            File file = new File(directory2, "file" + i + ".txt");
+            try {
+                file.createNewFile();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        
+        // Create a list of directory entries for testing.
+        List<File> entries = new ArrayList<>(); 
+        entries.add(directory1);
+        entries.add(directory2);
+        
+        // Select up to 20 entries from each directory.
+        List<File> selectedEntries = selectEntries(entries, 20); 
+        
+        // Print out the selected entries for verification.
+        for (File entry : selectedEntries) { 
+            System.out.println(entry.getName());
+        }
+    }
 }
--- src/main/java/ca/aidenw/engg1420/project/Split.java
+++ src/main/java/ca/aidenw/engg1420/project/Split.java
@@ -1,16 +1,105 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package ca.aidenw.engg1420.project;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+// Note: this code assumes that the input files are located in the same directory as the Java program.
 
 /**
+ * This Java code takes a list of files, splits each file into parts based 
+ *      on a specified number of lines, and creates new files for each part. 
+ *      The output is a list of the generated files.
  * @author Lucy
- * @author Leonardo
- * @author Daniel
- * @author Vanessa
- * @author Aiden
  */
-public class Split extends ProcessingElement {
+public class Split {
     
+    /*
+    * Description: The splitFiles method takes a list of input files and 
+    * an integer lines as parameters. It reads each input file line by 
+    * line, and for each lines number of lines, it creates a new output 
+    * file. The output files are stored in a list.
+    */
+    public static List<File> splitFiles(List<File> inputFiles, int lines) throws IOException {
+        // create an empty list to store output files
+        List<File> outputFiles = new ArrayList<File>();
+        // iterate over each input file in the list
+        for (File inputFile : inputFiles) {
+            // ignore directories
+            if (inputFile.isDirectory()) {
+                continue;
+            }
+            // create a buffered reader to read the input file
+            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+            // initialize variables to keep track of the output files and part numbers
+            int partNumber = 1;
+            String line;
+            List<String> linesList = new ArrayList<String>();
+            // read each line of the input file
+            while ((line = reader.readLine()) != null) {
+                // add the line to the list of lines for the current part
+                linesList.add(line);
+                // if the list of lines has reached the specified number, create a new output file
+                if (linesList.size() == lines) {
+                    // create the output file with the appropriate name
+                    String outputFileName = inputFile.getName() + ".part" + partNumber + ".txt";
+                    File outputFile = new File(outputFileName);
+                    // create a file writer to write to the output file
+                    FileWriter writer = new FileWriter(outputFile);
+                    // write each line to the output file
+                    for (String outputLine : linesList) {
+                        writer.write(outputLine + System.getProperty("line.separator"));
+                    }
+                    // close the writer and add the output file to the list
+                    writer.close();
+                    outputFiles.add(outputFile);
+                    // increment the part number and clear the list of lines
+                    partNumber++;
+                    linesList.clear();
+                }
+            }
+            // if there are remaining lines in the list, create a new output file
+            if (!linesList.isEmpty()) {
+                // create the output file with the appropriate name
+                String outputFileName = inputFile.getName() + ".part" + partNumber + ".txt";
+                File outputFile = new File(outputFileName);
+                // create a file writer to write to the output file
+                FileWriter writer = new FileWriter(outputFile);
+                // write each line to the output file
+                for (String outputLine : linesList) {
+                    writer.write(outputLine + System.getProperty("line.separator"));
+                }
+                // close the writer and add the output file to the list
+                writer.close();
+                outputFiles.add(outputFile);
+            }
+            // close the reader for the current input file
+            reader.close();
+        }
+        // return the list of output files
+        return outputFiles;
+    }
+
+    /*
+    * The main method is an example usage of the splitFiles method, where two input files 
+    * "File1.txt" and "File2.txt" are created and the value of lines is set to 10.
+    * The output file names are printed to the console for verification purposes.
+    */
+    public static void main(String[] args) throws IOException {
+        // create a list of input files
+        List<File> inputFiles = new ArrayList<File>();
+        inputFiles.add(new File("File1.txt"));
+        inputFiles.add(new File("File2.txt"));
+        // set the number of lines for each part
+        int lines = 10;
+        // call the splitFiles method with the input files and lines
+        List<File> outputFiles = splitFiles(inputFiles, lines);
+        // print the names of the output files
+        for (File outputFile : outputFiles) {
+            System.out.println(outputFile.getName());
+        }
+    }
+
 }

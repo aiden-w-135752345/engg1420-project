a0887024b549584ec7a93722a02f1d90ea8a23cc
aiden-w-135752345 <38215347+aiden-w-135752345@users.noreply.github.com>
2023-04-05 15:19:05 -0400

    CountFilter extends Filter

--- src/main/java/ca/aidenw/engg1420/project/CountFilter.java
+++ src/main/java/ca/aidenw/engg1420/project/CountFilter.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ca.aidenw.engg1420.project;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -15,35 +16,24 @@ import java.util.List;
  * @author Vanessa
  * @author Aiden
  */
-public class CountFilter extends ProcessingElement {
-    private static int count = 0;
-    public static Entry[] content(Entry[] entries,String key, int Min){
-        /* empty list to be appended to with the entries containing the key */
-        List<Entry> list = new ArrayList<>();
-        for (Entry entry : entries){
+public class CountFilter extends Filter {
+    @JsonProperty("Key")
+    private String key;
+    @JsonProperty("Min")
+    private int min;
+    @Override
+    public boolean condition(Entry entry){
+            int count = 0;
             /* read the entries line by line to search for the key */
-            List<String> allLines = Arrays.asList(entry.fileContents());
-
-            for (String line : allLines) {
+            for (String line : entry.fileContents()) {
                 /* if the line of text contains the key string */
                 if(line.contains(key)){
                     // only add the entry to the list if the count = the Min number of keys
-                    count += 1;
-                    if(count == Min){
-                        list.add(entry); // add the entry to the list of entries
-                        count = 0; // reset the count
-                        break;
+                    if(++count == min){
+                        return true;
                     }
                 }
             }
-        }
-        /* reformat the list back into an array to standardize output */
-        Entry[] output = list.toArray(new Entry[0]);
-        return output;
-    }
-    @Override
-    protected void accept(Entry entry) {
-        throw new UnsupportedOperationException("Unimplemented method 'accept'");
-    }     
-    
+        return false;
+    }    
 }

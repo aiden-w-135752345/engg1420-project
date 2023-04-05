843b067e32c590224f3cbc52d4c0be85d807b914
lalhajal <116668554+lalhajal@users.noreply.github.com>
2023-04-05 14:13:09 -0400

    added package line

--- src/main/java/ca/aidenw/engg1420/project/ListFiles.java
+++ src/main/java/ca/aidenw/engg1420/project/ListFiles.java
@@ -1,3 +1,5 @@
+package ca.aidenw.engg1420.project;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/ca/aidenw/engg1420/project/Split.java
+++ src/main/java/ca/aidenw/engg1420/project/Split.java
@@ -1,3 +1,5 @@
+package ca.aidenw.engg1420.project;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;

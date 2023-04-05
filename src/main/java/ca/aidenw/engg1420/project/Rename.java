115d54e6ff54d1c156e8105fc7f6c7bf9db168c7
vvictore <vanessavictorekpe@gmail.com>
2023-04-05 13:08:01 -0400

    Added main for rename, for locally saved files

--- src/main/java/ca/aidenw/engg1420/project/Rename.java
+++ src/main/java/ca/aidenw/engg1420/project/Rename.java
@@ -4,6 +4,8 @@
  */
 package ca.aidenw.engg1420.project;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * @author Lucy
@@ -21,4 +23,26 @@ public class Rename extends ProcessingElement {
     entry.rename(entry.name()+suffix);
     next.accept(entry);
 }
+  /*Seperate main method for entries donloaded from the server.
+  *Changes will be made on the local system
+  */
+  public static void main(String[] argv) throws IOException {
+        //Path which the entries are saved on the local system
+        File folder = new File("\\Users\\16474\\OneDrive - University of Guelph\\Documents\\ENGG 1420\\Entries\\PrimeNumbers");
+        File[] listOfFiles = folder.listFiles();
+        //Desired suffix added to the end of the entries name
+        String Suffix = "_edited";
+        
+  //Loop that runs to ensure that all files in the entry are edited
+  for (int i = 0; i < listOfFiles.length; i++) {
+        if (listOfFiles[i].isFile()) {
+            String oldName = listOfFiles[i].getName();
+            String newName = oldName.replaceFirst("\\.([^\\.]+)$", Suffix + ".$1");
+            File oldFile = new File(folder, oldName);
+            File newFile = new File(folder, newName);
+            oldFile.renameTo(newFile);
+        }
+    }
+        System.out.println("conversion is done");
+    }
 }

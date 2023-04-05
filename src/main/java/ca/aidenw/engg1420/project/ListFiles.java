aa41708e1ab634e00d9bf17c6c9b5a3538613732
aiden-w-135752345 <38215347+aiden-w-135752345@users.noreply.github.com>
2023-04-05 15:28:02 -0400

    Multithreading in ListFiles and Split.

--- src/main/java/ca/aidenw/engg1420/project/ListFiles.java
+++ src/main/java/ca/aidenw/engg1420/project/ListFiles.java
@@ -1,12 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package ca.aidenw.engg1420.project;
 
-import java.io.File;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.ArrayList;
-import java.util.List;
-
+import java.util.concurrent.CompletableFuture;
 /**
- * This takes in a list of directory entries and a maximum value, 
- *      and returns a list of selected entries from each directory.
  * @author Lucy
  * @author Leonardo
  * @author Daniel
@@ -14,38 +15,16 @@ import java.util.List;
  * @author Aiden
  */
 public class ListFiles extends ProcessingElement {
-    /**
-     * This takes in a list of directory entries and an integer Max,
-     *      and returns a list of selected entries.
-     *
-     * @param entries The list of directory entries to process.
-     * @param Max     The maximum number of entries to select from each directory.
-     * @return A list of selected entries from the directories in the input list.
-     */
-    public static List<File> selectEntries(List<File> entries, int Max) {
-        // Initialize an empty list to hold the selected entries.
-        List<File> selectedEntries = new ArrayList<>(); 
-
-        // Loop through each entry in the input list.
-        for (File entry : entries) {
-            // Check if the entry is a directory.
-            if (entry.isDirectory()) {
-                // Get an array of all the files in the directory.
-                File[] files = entry.listFiles(); 
-
-                int numFiles = files.length;
-                // Determine the number of files to select from the directory.
-                int numSelectedFiles = Math.min(numFiles, Max); 
-                
-                // Loop through the selected files in the directory.
-                for (int i = 0; i < numSelectedFiles; i++) {
-                    // Add the selected file to the output list.
-                    selectedEntries.add(files[i]); 
-                }
-            }
-        }
-        
-        // Return the list of selected entries.
-        return selectedEntries; 
+    @JsonProperty("Max")
+    private int max;
+    @Override
+    protected void accept(Entry directory) {
+        if(max<=0||!directory.isDirectory()){return;}
+        ArrayList<CompletableFuture>cfs=new ArrayList<>();
+        directory.dirContents(entry->{
+            cfs.add(CompletableFuture.runAsync(()->next.accept(entry),Main.executor));
+            return cfs.size()<max;
+        });
+        CompletableFuture.allOf(cfs.toArray(CompletableFuture[]::new)).join();
     }
 }
--- src/main/java/ca/aidenw/engg1420/project/Split.java
+++ src/main/java/ca/aidenw/engg1420/project/Split.java
@@ -1,18 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package ca.aidenw.engg1420.project;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.ArrayList;
-import java.util.List;
-
+import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
 /**
- * This takes a list of files, 
- *      splits each file into parts based on a specified number of lines, 
- *      and creates new files for each part. 
- *      The output is a list of files.
  * @author Lucy
  * @author Leonardo
  * @author Daniel
@@ -20,71 +16,20 @@ import java.util.List;
  * @author Aiden
  */
 public class Split extends ProcessingElement {
-    
-    /*
-    * This takes a list of input files and an integer lines as parameters.
-    *       It reads each input file line by line,
-    *       and for each line, it creates a new output file. 
-    *       The output files are stored in a list.
-    */
-    public static List<File> splitFiles(List<File> inputFiles, int lines) throws IOException {
-        // create an empty list to store output files
-        List<File> outputFiles = new ArrayList<File>();
-        // iterate over each input file in the list
-        for (File inputFile : inputFiles) {
-            // ignore directories
-            if (inputFile.isDirectory()) {
-                continue;
-            }
-            // create a buffered reader to read the input file
-            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-            // initialize variables to keep track of the output files and part numbers
-            int partNumber = 1;
-            String line;
-            List<String> linesList = new ArrayList<String>();
-            // read each line of the input file
-            while ((line = reader.readLine()) != null) {
-                // add the line to the list of lines for the current part
-                linesList.add(line);
-                // if the list of lines has reached the specified number, create a new output file
-                if (linesList.size() == lines) {
-                    // create the output file with the appropriate name
-                    String outputFileName = inputFile.getName() + ".part" + partNumber + ".txt";
-                    File outputFile = new File(outputFileName);
-                    // create a file writer to write to the output file
-                    FileWriter writer = new FileWriter(outputFile);
-                    // write each line to the output file
-                    for (String outputLine : linesList) {
-                        writer.write(outputLine + System.getProperty("line.separator"));
-                    }
-                    // close the writer and add the output file to the list
-                    writer.close();
-                    outputFiles.add(outputFile);
-                    // increment the part number and clear the list of lines
-                    partNumber++;
-                    linesList.clear();
-                }
-            }
-            // if there are remaining lines in the list, create a new output file
-            if (!linesList.isEmpty()) {
-                // create the output file with the appropriate name
-                String outputFileName = inputFile.getName() + ".part" + partNumber + ".txt";
-                File outputFile = new File(outputFileName);
-                // create a file writer to write to the output file
-                FileWriter writer = new FileWriter(outputFile);
-                // write each line to the output file
-                for (String outputLine : linesList) {
-                    writer.write(outputLine + System.getProperty("line.separator"));
-                }
-                // close the writer and add the output file to the list
-                writer.close();
-                outputFiles.add(outputFile);
-            }
-            // close the reader for the current input file
-            reader.close();
+    @JsonProperty("Lines")
+    private int lines;
+    @Override
+    protected void accept(Entry file) {
+        if(file.isDirectory()){return;}
+        ArrayList<CompletableFuture>cfs=new ArrayList<>();
+        String name=file.name();
+        String[] contents=file.fileContents();
+        for(int i=0,j=0;j<contents.length;i++,j+=lines){
+            int ii=i,jj=j;
+            cfs.add(CompletableFuture.runAsync(()->next.accept(file.makeFile(
+                    name+".part"+ii,Arrays.copyOfRange(contents, jj, (int)Math.min(jj+lines,contents.length))
+            )),Main.executor));
         }
-        // return the list of output files
-        return outputFiles;
+        CompletableFuture.allOf(cfs.toArray(CompletableFuture[]::new)).join();
     }
-
 }
